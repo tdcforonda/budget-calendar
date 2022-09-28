99948aa81c53dd8a40dd6cfde67f4047ff0d0580
Jerome Oliver Angeles <jerome.oliver.angeles@gmail.com>
2022-09-28 23:53:28 +0800

    Ito na talaga

--- Main.cpp
+++ Main.cpp
@@ -7,8 +7,9 @@ int totalBudget, budgetDaily, dSize, wSize, mSize;
 //Strings
 string userName, titleDaily, choiceDaily, choiceWeekly, choiceMonthly;
 //Arrays
-string* nameDaily = NULL; 
-int* amountDaily = NULL;
+string* nameDaily = NULL; int* amountDaily = NULL;
+string* nameWeekly = NULL; int* amountWeekly = NULL;
+string* nameMonthly = NULL; int* amountMonthly = NULL;
 
 
 int main()
@@ -17,7 +18,7 @@ int main()
 
 //input! -> user details, budget, expense
 cout<<"Before using the program, please enter your name: "<<endl;
-cin >> userName;
+getline(cin, userName);
 
 cout<<"Good day "<< userName << "! Welcome to Team Uno's Machine Problem. This Budget Planner program will use your Monthly Budget and daily, weekly, or monthly expenses in order to calculate a budget plan for you!"<< endl;
 
@@ -30,7 +31,7 @@ cin >> totalBudget;
 
     cout << "\nDo you want to add categories to your Daily Expenses? (Y/N) ";
     cin >> choiceDaily;
-    if (choiceDaily == "Y" || "y")
+    if (choiceDaily == "Y" || choiceDaily == "y")
     {
         cout<< "\nEnter number of Daily Expenses. ";
         cin >> dSize;
@@ -45,30 +46,21 @@ cin >> totalBudget;
             cout<<"\nHow much is your daily allotment for " << nameDaily[i]<<"?" <<endl;
             cin >> amountDaily [i];
         }
-   
-    
-    cout<<"You have "<< dSize<< " daily expenses, and these are:"<< endl;
     }
-<<<<<<< HEAD
-        for (int i = 0; i<dSize; i++)
-=======
-
-for (int i = 0; i<dSize; i++)
->>>>>>> 659b185db3adfa10ecc6d0d707db6fe6e116e734
+    else
     {
-        cout<< nameDaily[i]<<": "<< amountDaily[i]<< " Pesos"<<endl; 
+        cout<<"edi wow!";
     }
-/*
 //Weekly Expenses
     
     cout << "\nDo you want to add categories to your Weekly Expenses? (Y/N) ";
     cin >> choiceWeekly;
-    if (choiceWeekly == "Y" || "y")
+    if (choiceWeekly == "Y" || choiceWeekly == "y")
     {
         cout<< "\nEnter number of Weekly Expenses. ";
         cin >> wSize;
-        string* nameWeekly = new string[wSize];
-        int* amountWeekly = new int[wSize];
+        nameWeekly = new string[wSize];
+        amountWeekly = new int[wSize];
         cout<< endl;
 
         for (int i = 0; i<wSize; i++)
@@ -84,12 +76,12 @@ for (int i = 0; i<dSize; i++)
     
     cout << "\nDo you want to add categories to your Monthly Expenses? (Y/N) ";
     cin >> choiceMonthly;
-    if (choiceMonthly == "Y" || "y")
+    if (choiceMonthly == "Y" || choiceMonthly == "y")
     {
         cout<< "\nEnter number of Monthly Expenses. ";
         cin >> mSize;
-        string* nameMonthly = new string[mSize];
-        int* amountMonthly = new int[mSize];
+        nameMonthly = new string[mSize];
+        amountMonthly = new int[mSize];
         cout<< endl;
 
         for (int i = 0; i<mSize; i++)
@@ -100,28 +92,28 @@ for (int i = 0; i<dSize; i++)
             cin >> amountMonthly [i];
         }
     }
-*//*
+
 //Output Test
     //Daily
+    cout<<"You have "<< dSize<< " daily expenses, and these are:"<< endl;
     for (int i = 0; i<dSize; i++)
     {
-        cout<<"You have "<< dSize<< " daily expenses, and these are:"<< endl;
         cout<< nameDaily[i]<<": "<< amountDaily[i]<< " Pesos"<<endl; 
     }
 
     //Weekly
+    cout<<"You have "<< wSize<< " weekly expenses, and these are:"<< endl;
     for (int i = 0; i<wSize; i++)
     {
-        cout<<"You have "<< wSize<< " weekly expenses, and these are:"<< endl;
         cout<< nameWeekly[i]<<": "<< amountWeekly[i]<< " Pesos"<<endl; 
     }
 
      //Monthly
+    cout<<"You have "<< mSize<< " monthly expenses, and these are:"<< endl;
     for (int i = 0; i<mSize; i++)
     {
-        cout<<"You have "<< mSize<< " monthly expenses, and these are:"<< endl;
         cout<< nameMonthly[i]<<": "<< amountMonthly[i]<< " Pesos"<<endl; 
     }
-    */
+
 return 0;
 }
